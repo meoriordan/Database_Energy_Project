a60c3b09b6a9d21fdeae811cfdf4b71753320a4c
meoriordan <meoriordan@gmail.com>
2023-12-12 18:30:06 -0500

    add location add functionality

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
 from flask import Flask, render_template, request, url_for, redirect, session
+from datetime import date
 
 app = Flask(__name__)
 
@@ -74,16 +75,45 @@ def logout():
 
 @app.route('/locations', methods=('GET','POST'))
 def locations():
-    if session['loggedin'] == True:
-        cust_id = str(session['id'])
-        print('cust id issssss(', cust_id,')')
-        conn = get_db_connection() 
-        cur = conn.cursor() 
-        cur.execute('SELECT * FROM locations WHERE customer_id = %s', (cust_id))
-        locations = cur.fetchall()
-        return render_template("locations.html", locations=locations , nav_bar = session['loggedin'])
-    else:
-        return redirect(url_for('login'))
+    if request.method == 'GET':
+        if session['loggedin'] == True:
+            cust_id = str(session['id'])
+            print('cust id issssss(', cust_id,')')
+            conn = get_db_connection() 
+            cur = conn.cursor() 
+            cur.execute('SELECT * FROM locations WHERE customer_id = %s', (cust_id))
+            locations = cur.fetchall()
+            cur.close()
+            conn.close()
+            return render_template("locations.html", locations=locations , nav_bar = session['loggedin'])
+        else:
+            return redirect(url_for('login'))
+    elif request.method == 'POST':
+        street_num = request.form['street_num']
+        street_name = request.form['street_name']
+        apt_num = request.form['apt_num']
+        city = request.form['city']
+        state = request.form['state']
+        zipcode = request.form['zipcode']
+        num_br = request.form['num_br']
+        num_occupants = request.form['num_occupants']
+        sqft = request.form['sqft']
+        date_added = date.today()
+        customer_id = session['id']
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT max(location_id) FROM locations;')
+        location_id = cur.fetchall()[0][0]
+        location_id += 1
+        # print('CUSTOMER IDDDDD: ',customer_id)
+        cur.execute('INSERT INTO locations (location_id, customer_id,street_num, street_name, apt_num, city, state, zipcode, num_br, num_occupants, sqft, date_added)'
+            'VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+            (location_id, customer_id,street_num, street_name, apt_num, city, state, zipcode, num_br, num_occupants, sqft, date_added))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('locations'))
+
 
 @app.route('/devices')
 def devices():
@@ -95,7 +125,8 @@ def devices():
         devices = cur.fetchall()
         return render_template("devices.html", devices=devices, nav_bar = session['loggedin'])
     else:
-        return redirect(url_for('login'))    
+        return redirect(url_for('login'))   
+
 
 
 @app.route('/create', methods=('GET','POST'))
